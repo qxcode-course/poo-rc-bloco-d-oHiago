2e420086804fadaea02ef4becd5b588d6fddee74
oHiago <hiagobica@gmail.com>
2025-12-02 16:16:25 -0300

    add criado

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -9,18 +9,16 @@ class Fone:
 class Contact:
     def __init__(self,name:str):
         self.name = name
-        self.fones: list[Fone] = []
+        self.fones: list [Fone] = []
         self.favorited = False
 
     def addFone(self, fone: Fone):
-       self.fones.append(fone)
+        self.fones.append(fone)
 
     def remove(self, index: int):
-        if index < 0 or index > len(self.fones):
-            print("index invalido")
-        
-        else:
-            self.fones.pop(index)
+        if index < 0 or index >= len(self.fones):
+            raise Exception ("fail: index invalido")
+        self.fones.pop(index)
     def validar(self, fone: Fone) -> bool:
         validos = "0123456789()."
         for i in fone.number:
@@ -40,18 +38,24 @@ class Contact:
         
 class Agenda:
     def __init__(self):
-        self.contato: dict[str, Contact] = {}
+        self.contato: dict[Contact] = {}
 
-    def add_contato(self, contato: Contact):
-        if contato.name in self.contato:
-            raise Exception (f"Contato nome {contato.name} ja existe")
-        self.contato[contato.name] = contato
-
+    def add_contato(self,name, fones):
+        if name not in self.contato:
+          self.contato[name] = Contact(name)
+        for fone in fones:
+            self.contato[name].addFone(fone)
     def get_contato(self, nome:str):
-        try:
+         try:   
             return self.contato[nome]
-        except KeyError as _:
+         except KeyError as _:
             raise Exception(f"Contato nome {nome} nao existe")
+    
+        
+    def __str__(self):
+        lista = sorted(self.contato.values(), key = lambda c: c.name)
+        return "\n".join(str(contato) for contato in lista)
+
         
 def main():
 
@@ -68,7 +72,16 @@ def main():
             elif args[0] == "show":
                 print(agenda)
             elif args[0] == "add": 
-                agenda.add_contato(Contact(args[1]))
+               name = args[1]
+               fone = []
+               for token in args[2:]:
+                   if ":" not in token:
+                       continue
+                   label, number = token.split(":")
+                   fone.append(Fone(label,number))
+               agenda.add_contato(name,fone)         
+            else:
+                print("fail: comando invalido")
         except Exception as e:
             print(e)
 if __name__ == "__main__":
